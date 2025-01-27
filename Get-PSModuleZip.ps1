6b2f92e0e5621a8cfc48a2437799dbbd35e6a178
Dirty REdOG <redog@users.noreply.github.com>
2025-01-27 14:41:56 -0600

    fixed the fixes

--- Get-PSModuleZip.ps1
+++ Get-PSModuleZip.ps1
@@ -8,34 +8,23 @@ function Get-PSModuleZip {
         [string]$PackageVersion
     )
 
-    # --- 1. Save the Module Locally (using /tmp) ---
-    $tempModulePath = "/tmp/ModuleUpdate/$PackageName" # Version NOT included
-    $tempModulePathWithVersion = "/tmp/ModuleUpdate/$PackageName-$PackageVersion"
-    
-		if (!(Test-Path -Path $tempModulePath)) {
+    $tempModulePath = "/tmp/ModuleUpdate_$PackageName"
+
+    if (!(Test-Path -Path $tempModulePath)) {
         New-Item -ItemType Directory -Path $tempModulePath -Force
     }
-    
-    if (!(Test-Path -Path $tempModulePathWithVersion)) {
-        New-Item -ItemType Directory -Path $tempModulePathWithVersion -Force
-    }
-    
+
     try {
-        Save-Module -Name $PackageName -Path $tempModulePathWithVersion -RequiredVersion $PackageVersion -Force -Verbose
+        Save-Module -Name $PackageName -Path $tempModulePath -RequiredVersion $PackageVersion -Force -Verbose
 
-        # Get the module directory (where Save-Module actually saved it)
-        $moduleDir = Get-ChildItem -Path $tempModulePathWithVersion | Where-Object {$_.PSIsContainer}
-
-        # Move module content to directory without version number
-        Move-Item -Path "$tempModulePathWithVersion/$moduleDir" -Destination $tempModulePath -Force
+        # No need to move anything, as we're saving directly to the desired location.
     }
     catch {
         Write-Error "An error occurred while saving the module: $_"
         return # Exit the function if there's an error
     }
 
-    # --- 2. Compress to Zip ---
-    $zipFilePath = "/tmp/$PackageName.zip" # Version NOT included in zip file name
+    $zipFilePath = "/tmp/$PackageName.zip"
     try {
         Compress-Archive -Path "$tempModulePath/*" -DestinationPath $zipFilePath -Force -Verbose
         Write-Host "Module '$PackageName' version '$PackageVersion' has been downloaded and zipped to '$zipFilePath'"
@@ -44,7 +33,5 @@ function Get-PSModuleZip {
         Write-Error "An error occurred while compressing the module: $_"
     }
 
-    # --- Cleanup (Optional) ---
     Remove-Item -Path $tempModulePath -Recurse -Force
-    Remove-Item -Path $tempModulePathWithVersion -Recurse -Force
 }
